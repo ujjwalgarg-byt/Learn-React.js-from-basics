8089e63bbf66e57f0759e0ef74de4a42767b896d
ujjwalgarg-byt <ujjwalgargofficial01@gmail.com>
2025-04-09 12:37:29 +0530

    new code

--- 01_basics/02_two.js
+++ 01_basics/02_two.js
@@ -2,16 +2,41 @@
 
 
 // here we create a h1 tag using react which is a core react thing
-const heading = React.createElement("h1",{id:"heading"},"hello world from react!");
+// const heading = React.createElement("h1",{id:"heading"},"hello world from react!");
 //in above code {} is used to give the attributes to the tags.
-console.log(heading);//object
+//console.log(heading);//object
 // here heading is a react element which is also a javascript object.
 
 // now we create a root element using react dom which is used to render the react component into the html page.
 // here we are using document.getElementById to get the root element from the html page.
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // now rendering h1 tag into root
-root.render(heading);
-
+// root.render(heading);
+//(ReactElement or object)=> HTML(browser understand)
 // this render method used to take heading as js object and convert it into html tag and put it into the root element.
+
+
+/** now create a nested html structure using react like below
+ * 
+ * <div id="parent">
+ *    <div id="child">
+ *        <h1>i am an h1 tag</h1>
+ *    </div>
+ * </div>
+ * 
+ */
+
+
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        React.createElement("h1",{},"i am an h1 tag")
+    )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
